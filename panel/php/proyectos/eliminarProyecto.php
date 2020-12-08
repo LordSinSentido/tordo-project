9d90acc2362d17945bc2c35f65e89d4810936468
LordSinSentido <misogu@live.com>
2020-12-08 08:53:01 -0600

    Actualización eliminar en proyectos

--- panel/php/proyectos/eliminarProyecto.php
+++ panel/php/proyectos/eliminarProyecto.php
@@ -3,13 +3,18 @@
     
     $id = $_POST['id'];
 
-    $tomarImagen = "SELECT imagen1 FROM proyectos WHERE id = $id";
+    $tomarImagen = "SELECT imagen1, imagen2, imagen3 FROM proyectos WHERE id = $id";
     $query = $conexion->query($tomarImagen);
     $nombre = mysqli_fetch_array($query);
 
     $ruta = "../../../inicio/img/proyectos";
-    $ruta = $ruta . '/' . $nombre[0];
-    unlink($ruta);
+    $ruta1 = $ruta . '/' . $nombre[0];
+    $ruta2 = $ruta . '/' . $nombre[1];
+    $ruta3 = $ruta . '/' . $nombre[2];
+    
+    unlink($ruta1);
+    unlink($ruta2);
+    unlink($ruta3);
 
     $eliminar = "DELETE FROM proyectos WHERE id = '$id'";
 
